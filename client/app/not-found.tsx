2e77b6cf639e81a48dc3c5f443563570ce497b21
Ernest-Yoyowah <ernestniiyoyowah@gmail.com>
2024-12-13 17:38:18 +0000

    Remove node_modules and package-lock.json from tracking

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -1,5 +1,5 @@
 import CustomButton from "@/components/common/customButton";
-import { Box, Typography, Button } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const NotFound = () => {
   return (
