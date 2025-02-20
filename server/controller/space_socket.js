f7e8963cc48fd96a11ff4463c2c073a4cfc2b675
AJ <121802680+aj-codess@users.noreply.github.com>
2025-02-20 09:49:35 -0500

    Update space_socket.js

--- server/controller/space_socket.js
+++ server/controller/space_socket.js
@@ -5,7 +5,7 @@ import log_service from "./../service/logService.js"
 const ws = new WebSocket.Server({noServer:true});
 
 //ws - websocket, wsc - websocket client
-ws.on("connection",(request,wsc)=>{
+ws.on("connection",(wsc,request)=>{
 
     wsc.send(JSON.stringify({ status: "success", message: "connected to space" }));
 
